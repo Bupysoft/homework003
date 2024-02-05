508be9554498ee37ac8609ee4ea7c419e08f4554
Bupy <bupysoft@gmail.com>
2024-02-05 17:52:29 +0200

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+
+    public int addition(int num1, int num2) {
+        int i = num1 + num2;
+        return i;
+    }
+    public boolean isGreater (double num3, double num4){
+        boolean isNumOneBiggerNumTwo=num3>num4;
+        return isNumOneBiggerNumTwo;
+    }
+}
+   /* public int isGreater(){
+        System.out.println("Engine has been stopped");
+    }*/
+
---
+++ src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    String brand;
+    String model;
+    int year;
+
+
+    public void startEngine(){
+        System.out.println("Engine has been started");
+    }
+    public void stopEngine(){
+
+        System.out.println("Engine has been stopped");
+    }
+
+}
